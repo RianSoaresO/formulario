f0e01f111e7280dd24fa57f8a76363b95b0a0a89
RianSoaresO <riamsoares556@gmail.com>
2025-02-24 21:44:50 -0300

    formulario

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('btnInscrever').addEventListener('click', function() {
+    var email = document.getElementById('inputEmail4').value;  // Captura o valor do campo de entrada de e-mail
+    alert("O seu e-mail é: " + email);  // Exibe o valor do e-mail em um alerta
+});
